52dccc86cef9c98a6ff290f6b9971c2483656887
Macarena061199 <102336411+Macarena061199@users.noreply.github.com>
2022-04-03 22:14:53 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuiaEjercicio16
+{
+    internal class Ejercicio
+    {
+        /* 16.	Solicite un ingreso al usuario y determine si puede ser interpretado como un número entero.
+        (De aquí en más todos los ingresos del usuario deben ser verificados, finalizando el programa con un 
+        mensaje de error en caso de ser inválidos)*/
+
+        public void Ejercicio16()
+        {
+            bool flag = false;
+            do
+            {
+                Console.Write("Por favor ingrese un número entero: ");
+                string ingreso = Console.ReadLine();
+                flag = ValidadNumero(ingreso);
+
+                if(flag==true)
+                {
+                    Console.WriteLine($"El número ingresado fue {ingreso}.");
+                }
+               
+            }while(flag==false);
+            
+
+            
+        }
+
+        private bool ValidadNumero(string ingreso)
+        {
+            bool flag = false;
+            if(!int.TryParse(ingreso, out int salida))
+            {
+                Console.WriteLine("Por favor ingrese un valor numérico.");
+            }
+            else if(salida < 0)
+            {
+                Console.WriteLine("Por favor ingrese un número posiivo.");
+            }
+            else
+            {
+                flag=true;
+            }
+            return flag;
+
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GuiaEjercicio16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicio e = new Ejercicio();
+            e.Ejercicio16();
+        }
+    }
+}
